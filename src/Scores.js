45629f599679f0ad2049ad0701734e32c82c5bb8
Blake-Franzen <bfranzen@uw.edu>
2018-03-11 15:52:33 -0700

    fixing merge issue

--- src/Scores.js
+++ src/Scores.js
@@ -1,7 +1,6 @@
-
 import React, { Component } from 'react';
 import firebase from 'firebase';
-
+import {RadialBarChart, RadialBar} from 'recharts';
 // Component representing the scores page
 export class Scores extends Component {
     constructor(props) {
