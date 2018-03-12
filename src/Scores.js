e82c38f618aea5c816a1fccddadde5a59c851427
jmatray <jmatray@uw.edu>
2018-03-11 21:46:03 -0700

    More work on radialgraph

--- src/Scores.js
+++ src/Scores.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-<<<<<<< HEAD
-import  { RadialBarChart, RadialBar, Cell, Tooltip, Legend } from 'recharts';
-=======
 import  { RadialBarChart, RadialBar, Label, LabelList } from 'recharts';
->>>>>>> 8b83d813b4b627d6aee5cd225e833d59a6bdbf37
 import * as d3 from 'd3';
 
 // Component representing the scores page
@@ -49,8 +45,9 @@ export class Scores extends Component {
                 <div className="charts">
                     {this.state.game === "Snake" && <Charts name="SnakeScores" snapshotToArray={(snapshot) => this.snapshotToArray(snapshot)}/>}
                     {this.state.game === "Reacteroids" && <Charts name="ReacteroidsScores" snapshotToArray={(snapshot) => this.snapshotToArray(snapshot)}/>}
-                    {this.state.game === "Reacteroids" && <Charts name="FifteenPuzzleScores" snapshotToArray={(snapshot) => this.snapshotToArray(snapshot)}/>}
+                    {this.state.game === "FifteenPuzzle" && <Charts name="FifteenPuzzleScores" snapshotToArray={(snapshot) => this.snapshotToArray(snapshot)}/>}
                 </div>
+                <Radial snapshotToArray={(snapshot) => this.snapshotToArray(snapshot)}/>
             </div>
         )
     }
@@ -69,13 +66,15 @@ class Charts extends Component {
     // On mount, pull from the "scores" table in the database
     componentDidMount() {
         let ref = firebase.database().ref(this.props.name);
-        let db = ref.orderByChild("score").limitToLast(10);
-        db.on('value', (snapshot => {
-            let dat = this.props.snapshotToArray(snapshot);
-            this.setState({ scoreData: dat });
-        }));
         let name = firebase.auth().currentUser.displayName;
-        this.setState({user: name});
+
+        let scores = ref.orderByChild("score");
+        scores.on('value', (snapshot => {
+            let dat = this.props.snapshotToArray(snapshot);
+            this.setState({ scoreData: dat, user: name });
+        }));
+        
+
     }
 
     getUserData(array) {
@@ -89,19 +88,37 @@ class Charts extends Component {
     }
 
     render() {
+        let userScores = [];
+        this.state.scoreData.forEach((d) => {
+           if(d.name === this.state.user) {
+               userScores.push(d);
+           }
+        })
+
+        let topTen = [];
+        let amount = 0;
+        if (this.state.scoreData.length < 10) {
+            amount = this.state.scoreData.length;
+        } else {
+            amount = 10;
+        }
+        for(let i = 0; i < amount; i++) {
+            if (this.state.scoreData[i].name !== this.state.user) {
+                userScores.push(this.state.scoreData[i]);
+            }
+            topTen.push(this.state.scoreData[i]);
+        }
          let radialData = d3.nest()
          .key(function(d) { return d.name;})
          .rollup(function(v) { return d3.mean(v, function (d) { return d.score;})})
-         .entries(this.state.scoreData);
-<<<<<<< HEAD
-         console.log(radialData); 
-
-         const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-         
-=======
-         console.log(radialData);  
+         .entries(userScores);
          this.getUserData(radialData);   
->>>>>>> 8b83d813b4b627d6aee5cd225e833d59a6bdbf37
+
+        console.log(this.state.scoreData);
+
+
+        
+
         return (
             <div className="charts-container">
                 <table>
@@ -112,7 +129,7 @@ class Charts extends Component {
                             <th>Score</th>
                         </tr>
                         {
-                            this.state.scoreData.map((d, i) => {
+                            topTen.map((d, i) => {
                                 return (
                                     <tr key={'item-' + i}>
                                         <td>{i + 1}</td>
@@ -124,116 +141,13 @@ class Charts extends Component {
                         }
                     </tbody>
                 </table>
-<<<<<<< HEAD
-                <RadialBarChart width={730} height={250} innerRadius="10%" outerRadius="80%" data={radialData} startAngle={180} endAngle={0}>
-                    <RadialBar minAngle={15} label={{ fill: '#666', position: 'insideStart' }} background clockWise={true} dataKey='value'>
-                        {
-                            radialData.map((entry, index) => {
-                                const color = entry.value > 4000 ? COLORS[0] : COLORS[1];
-                                return <Cell fill={color} />;
-                            })
-                        }
-                    </RadialBar>
-                    <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" />
-                    <Tooltip />
-=======
                 <RadialBarChart width={750} height={750} innerRadius="10%" outerRadius="80%" data={radialData} startAngle={180} endAngle={0}>
                     <RadialBar minAngle={15} background clockWise={true} dataKey='value' >
                     <LabelList dataKey="key" fill="#EEE"/>
                     </RadialBar>
->>>>>>> 8b83d813b4b627d6aee5cd225e833d59a6bdbf37
                 </RadialBarChart>
             </div>
         )
     }
-<<<<<<< HEAD
 }
 
-// Component representing data for Reacteroids game
-class ReacteroidsScores extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            scoreData: []
-        }
-    }
-    // On mount, pull from the "scores" table in the database
-    componentDidMount() {
-        let ref = firebase.database().ref('ReacteroidsScores');
-        let db = ref.orderByChild("score").limitToLast(10);
-        db.on('value', (snapshot => {
-            let dat = this.props.snapshotToArray(snapshot);
-            this.setState({ scoreData: dat });
-        }));
-    }
-
-    render() {
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Username</th>
-                        <th>Score</th>
-                    </tr>
-                    {
-                        this.state.scoreData.map((d, i) => {
-                            return (
-                                <tr key={'item-' + i}>
-                                    <td>{i + 1}</td>
-                                    <td>{Object.entries(d)[0][1]}</td>
-                                    <td>{Object.entries(d)[1][1]}</td>
-                                </tr>
-                            );
-                        })
-                    }
-                </tbody>
-            </table>
-        )
-    }
-}
-
-class FifteenPuzzleScores extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            scoreData: []
-        }
-    }
-    // On mount, pull from the "scores" table in the database
-    componentDidMount() {
-        let ref = firebase.database().ref('FifteenPuzzleScores');
-        let db = ref.orderByChild("score").limitToLast(10);
-        db.on('value', (snapshot => {
-            let dat = this.props.snapshotToArray(snapshot);
-            this.setState({ scoreData: dat });
-        }));
-    }
-
-    render() {
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Username</th>
-                        <th>Score</th>
-                    </tr>
-                    {
-                        this.state.scoreData.map((d, i) => {
-                            return (
-                                <tr key={'item-' + i}>
-                                    <td>{i + 1}</td>
-                                    <td>{Object.entries(d)[0][1]}</td>
-                                    <td>{Object.entries(d)[1][1]}</td>
-                                </tr>
-                            );
-                        })
-                    }
-                </tbody>
-            </table>
-        )
-    }
-=======
->>>>>>> 8b83d813b4b627d6aee5cd225e833d59a6bdbf37
-}
